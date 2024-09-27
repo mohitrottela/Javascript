057dd9d6cc1c30ba19cb2b0976bf98bd0a8fe7e1
mohitrottela <mohitmahirottela@gmail.com>
2024-09-27 15:17:24 +0530

    for loop

---
+++ loopAndStrings.js
@@ -0,0 +1,17 @@
+//for loop
+
+// for(let i= 0; i<7; i++){
+//     console.log("print value =", i);
+// }
+
+// // calculat 1 to 10
+
+// let sum =0 ;
+// let num =prompt("enter value ");
+// for(let i=0;i<=num;i++){
+//     sum += i;
+// }
+// console.log(sum);
+
+// While loop
+
