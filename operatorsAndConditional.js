6e003a2102c538b106fb0ef4735336ebaa212646
mohitrottela <mohitmahirottela@gmail.com>
2024-09-27 08:26:02 +0530

    arithmetic operators

---
+++ operatorsAndConditional.js
@@ -0,0 +1,29 @@
+// Arithmetic operators
+
+let a = 10;
+let b = 2;
+
+//adding
+console.log("a + b =",a+b);
+//subtract
+console.log("a - b =",a-b);
+//multiple
+console.log("a x b =",a*b);
+//divide
+console.log("a / b =",a/b);
+
+//modulus
+console.log("a % b =", a%b);
+
+//exponential
+console.log("a ** b =", a**b);
+
+//increment
+console.log('a++ =', a++);
+console.log("++a =",++a);
+console.log(a)
+
+// decrement
+console.log('b-- =', b--);
+console.log('--b =', --b);
+console.log('b+4 =', b+4)
