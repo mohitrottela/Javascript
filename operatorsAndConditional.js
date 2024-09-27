8b7aff9ec329d5052b78b9d8654a5606f62949f1
mohitrottela <mohitmahirottela@gmail.com>
2024-09-27 08:37:26 +0530

    comparison operator done

--- operatorsAndConditional.js
+++ operatorsAndConditional.js
@@ -1,29 +1,35 @@
-// Arithmetic operators
+// // Arithmetic operators
 
-let a = 10;
+// let a = 10;
+// let b = 2;
+
+// //adding
+// console.log("a + b =",a+b);
+// //subtract
+// console.log("a - b =",a-b);
+// //multiple
+// console.log("a x b =",a*b);
+// //divide
+// console.log("a / b =",a/b);
+
+// //modulus
+// console.log("a % b =", a%b);
+
+// //exponential
+// console.log("a ** b =", a**b);
+
+// //increment
+// console.log('a++ =', a++);
+// console.log("++a =",++a);
+// console.log(a)
+
+// // decrement
+// console.log('b-- =', b--);
+// console.log('--b =', --b);
+// console.log('b+4 =', b+4);
+
+// comparison operator
+let a = 2;
 let b = 2;
 
-//adding
-console.log("a + b =",a+b);
-//subtract
-console.log("a - b =",a-b);
-//multiple
-console.log("a x b =",a*b);
-//divide
-console.log("a / b =",a/b);
-
-//modulus
-console.log("a % b =", a%b);
-
-//exponential
-console.log("a ** b =", a**b);
-
-//increment
-console.log('a++ =', a++);
-console.log("++a =",++a);
-console.log(a)
-
-// decrement
-console.log('b-- =', b--);
-console.log('--b =', --b);
-console.log('b+4 =', b+4)
+console.log("a >= b", a >= b);//true
