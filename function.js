00ba1c605dfa4c8b9332458668446109e66f31ab
mohitrottela <mohitmahirottela@gmail.com>
2024-09-30 16:46:43 +0530

    foreach and map method

---
+++ function.js
@@ -0,0 +1,36 @@
+let count = 0;
+function findVowel(par){
+    for(value of par){
+        if(value ==='a'|| value ==='e'|| value ==='i'|| value ==='o'|| value ==='u'){
+            count ++;
+        }
+    }
+    return count;
+}
+// console.log(findVowel('mohit'));
+
+//arrow function
+let arrcount =0;
+const arrFind = (par)=>{
+    for(value of par){
+        if(value ==='a'|| value ==='e'|| value ==='i'|| value ==='o'|| value ==='u'){
+            arrcount ++;
+        }
+    }
+    return arrcount;
+}
+// console.log(arrFind('himachal'))
+
+//foreach function
+
+let arr1 = [2,5,8];
+arr1.forEach(function(val){
+    console.log(val*val)
+})
+
+//map method
+
+let cube =arr1.map((val)=>{
+   return val**3;
+})
+console.log(cube);
