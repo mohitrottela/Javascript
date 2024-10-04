9368c0d7e6d828ac95b394bbcb01c65d7d371c38
mohitrottela <mohitmahirottela@gmail.com>
2024-10-04 20:57:17 +0530

    document and query selector use

---
+++ Dom.js
@@ -0,0 +1,15 @@
+// selecting with ID
+let myId = document.getElementById('myId');
+console.dir(myId);
+
+//selecting with Class
+let myClass = document.getElementsByClassName("myClass");
+console.dir(myClass);
+
+let par = document.getElementsByTagName('p');
+console.dir(par);
+
+// querySelector
+
+let query = document.querySelector(".myClass");
+console.dir(query);
