e99c5152daea38d55e4eb2e1f755c3222812e1af
mohitrottela <mohitmahirottela@gmail.com>
2024-10-08 18:07:03 +0530

    dom first part compeleted

--- Dom.js
+++ Dom.js
@@ -13,3 +13,13 @@ console.dir(par);
 
 let query = document.querySelector(".myClass");
 console.dir(query);
+
+let boxs = document.querySelectorAll('.box');
+// boxs[0].innerText = 'This is not first div'
+// boxs[1].innerText = 'This is not second div'
+// boxs[2].innerText = 'This is not third div'
+let i = 0;
+for(box of boxs){
+    box.innerText = 'enter the value of node div index ' +i;
+    i++;
+}
